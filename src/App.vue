e21972be7a159fde6ccef22b87f47bd52bdc83fc
lonely <3060900582@qq.com>
2025-01-02 12:42:46 +0800

    feat(mock): init mock

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,19 @@
 
 <script lang="ts" setup>
 import testC from '@/components/testC.vue';
+import axios from 'axios';
+import { onMounted } from 'vue';
 
+onMounted(() => {
+  login();
+});
+const login = async () => {
+  let data = await axios.post('/api/user/login', {
+    username: 'admin',
+    password: '111111',
+  });
+  console.log(data);
+};
 console.log(import.meta.env);
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,39 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { viteMockServe } from 'vite-plugin-mock';
 import path from 'path'; // 导入 path 模块
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // Specify symbolId format
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnable: true,
-        additionalData: '@use "@/assets/styles/variable.scss" as *;',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      // 全局icon
+      createSvgIconsPlugin({
+        // Specify the icon folder to be cached
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // Specify symbolId format
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      // 模拟数据
+      viteMockServe({
+        localEnabled: command === 'serve',
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnable: true,
+          additionalData: '@use "@/assets/styles/variable.scss" as *;',
+        },
       },
     },
-  },
-  resolve: {
-    // 别名
-    alias: {
-      '@': path.resolve('./src'),
+    resolve: {
+      // 别名
+      alias: {
+        '@': path.resolve('./src'),
+      },
     },
-  },
+  };
 });
